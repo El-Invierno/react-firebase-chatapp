d8697f4af50e5c11ce04076b9de3f85111787212
Yash Tawde <yashtawde9@gmail.com>
2024-08-26 11:50:16 +0530

    Made the base structure without functionality.

---
+++ src/components/list/chatList/Chatlist.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./chatList.css"
+
+const Chatlist = () => {
+  return (
+    <div className='chatList'>
+      <div className="search">
+        
+      </div>
+    </div>
+  )
+}
+
+export default Chatlist
